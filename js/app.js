cd8bd997eaaa74bc08dadbb7cf93651bda066129
Zack Klinger <zklinger@gmail.com>
2016-04-14 23:16:30 -0700

    STYLE layouts for all devices and ADD jQuery overlay

--- js/app.js
+++ js/app.js
@@ -1,13 +1,14 @@
 $(document).ready(function() {
   var $overlay = $('<div id="overlay"></div>');
   var $image = $('<img>');
+  var $caption = $('<p></p>');
   
+  // Add image to overlay
   $overlay.append($image);
-  
+  // Add caption to overlay
+  $overlay.append($caption);
   // Add overlay
   $('body').append($overlay);
-    // image
-    // caption
   
   // Capture click event
   $('#photoGallery a').click(function(event) {
@@ -15,9 +16,11 @@ $(document).ready(function() {
     var imageSource = $(this).attr('href');
     // Update overlay with clicked image
     $image.attr('src', imageSource);
+    // Get child's alt attr and set caption
+    var captionText = $(this).children('img').attr('alt');
+    $caption.text(captionText);
     // Show the overlay
     $overlay.show();    
-    // Get child's alt attr and set caption
   });
   // When overlay is clicked
   $overlay.click(function() {
