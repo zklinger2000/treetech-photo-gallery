48f5466d59ad9d8e85f55d483863c3ea9a7f002f
Zack Klinger <zklinger@gmail.com>
2016-04-14 21:35:06 -0700

    ADD jQuery to image links

---
+++ js/app.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+  // Capture click event
+  $('#photoGallery a').click(function(event) {
+    event.preventDefault();
+    var $href = $(this).attr('href');
+    console.log($href);
+  });
+    // Show the overlay
+    // Update overlay with clicked image
+    // Get child's alt attr and set caption
+  // Add overlay
+    // image
+    // caption
+  // When overlay is clicked
+    // Hide overlay
+});
