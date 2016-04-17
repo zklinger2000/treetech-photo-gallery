16bca5002fd08f658b19212398bb89301bf2b3da
Zack Klinger <zklinger@gmail.com>
2016-04-16 20:54:11 -0700

    UPDATE logic for prev/next arrows

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-  var photos = [{
+var photos = [{
     "src": "01.jpg",
     "name": "Hay Bales",
     "title": "I love hay bales. Took this snap on a drive through the countryside past some straw fields."
@@ -49,8 +49,11 @@
   }];  
 
 $(document).ready(function() {
+  'use strict';
+  var $photoGallery = $('#photoGallery');
   var $overlay = $('<div id="overlay"></div>');
-//  var $prevPhoto = $('<div id="prevPhoto"><</div>');
+  var $prevPhoto = $('<div id="prevPhoto"><</div>');
+  var $nextPhoto = $('<div id="nextPhoto">></div>');
   var $image = $('<img>');
   var $caption = $('<p></p>');
   
@@ -60,10 +63,12 @@ $(document).ready(function() {
   photos.forEach(function(photo) {
     galleryHtml += '<a href="photos/' + photo.src + '"><img src="photos/thumbnails/' + photo.src + '" alt="Photo of ' + photo.name + '" title="' + photo.title + '"></a>'
   });
-  $('#photoGallery').html(galleryHtml);
+  $photoGallery.html(galleryHtml);
   
-  // Add prevPhoto to overlay
-//  $overlay.append($prevPhoto);
+  // Add prevPhoto to #photoGallery
+  $photoGallery.append($prevPhoto);
+  // Add nextPhoto to #photoGallery
+  $photoGallery.append($nextPhoto);
   // Add image to overlay
   $overlay.append($image);
   // Add caption to overlay
@@ -84,6 +89,7 @@ $(document).ready(function() {
     $overlay.fadeIn('slow', function() {
       $overlay.children('img').fadeTo('slow', 1, function() {
         $('#prevPhoto').fadeTo('slow', 1);
+        $('#nextPhoto').fadeTo('slow', 1);
         $overlay.children('p').fadeTo('fast', 1);
       });
     });
@@ -91,6 +97,7 @@ $(document).ready(function() {
   // When overlay is clicked
   $overlay.click(function() {
     $('#prevPhoto').fadeTo(1, 0);
+    $('#nextPhoto').fadeTo(1, 0);
     // Hide overlay
     $overlay.fadeOut('fast', function() {
       $overlay.children('img').fadeTo(1, 0, function() {
@@ -98,16 +105,40 @@ $(document).ready(function() {
       });
     });
   });
-  // Click event for next image
-  $('#prevPhoto').click(function(event) {
-    event.preventDefault();
-    var imageSource = $('#overlay img').attr('src');
+  // Click event for prev photo in visible series
+  $('#prevPhoto').click(['reverse'], changeImage);
+  // Click event for next photo in visible series
+  $('#nextPhoto').click([], changeImage);
+  
+  function changeImage(event) {
+    var args = Array.prototype.slice.call(arguments);
 
-    console.log(photos.findIndex(function(photo) {
-      return photo.src === imageSource.slice(7);
-    }));
-  });
-      
+    // Prevent the link from loading the image location
+    event.preventDefault();
+    // Get list of photos still visible after search filter
+    var $anchors = $('#photoGallery a').filter(function(index, element) {
+      return $(element).css('display') === 'block';
+    });
+    // Do nothing if there's only 1 photo
+    if ($anchors.length < 2) return;
+    // Get photo filename
+    var imageSource = $('#overlay img').attr('src').slice(7);
+    // Get anchor element that matches current photo
+    var $current = $anchors.find('img').filter(function(i, photo) {
+      return ($(photo).attr('src').slice(18) === imageSource);
+    });
+    var currentIndex = $anchors.index($current.parent());
+    // Get index of next/prev anchor element
+    var newIndex = (args[0].data[0] === 'reverse' ? 
+                    (currentIndex === 0 ? $anchors.length - 1 : currentIndex - 1) : 
+                    (currentIndex === $anchors.length - 1 ? 0 : currentIndex + 1));
+    // Update overlay with clicked image
+    $image.attr('src', 'photos/' + $($anchors.get(newIndex)).find('img').attr('src').slice(18));
+    // Get child's alt attr and set caption
+    var captionText = $($anchors.get(newIndex)).find('img').attr('title');
+    $caption.text(captionText);
+  }
+  
   function sortByHref(a, b) {
     var aSrc = $(a).attr('href');
     var bSrc = $(b).attr('href');
@@ -122,7 +153,7 @@ $(document).ready(function() {
     }).parent().css('display', 'none').sort(sortByHref).appendTo($('#photoGallery'));
     $('#photoGallery a img').filter(function(index, element){
       return $(element).attr('title').toLowerCase().includes((event.currentTarget.value.toLowerCase()));
-    }).parent().css('display', 'inherit').sort(sortByHref).prependTo($('#photoGallery'));
+    }).parent().css('display', 'block').sort(sortByHref).prependTo($('#photoGallery'));
   });
   
 });
